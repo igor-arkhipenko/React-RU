fb8cd30c5826557708ffd0775471ebc2b9a091bb
Igor Arkhipenko <qloney@mail.ru>
2017-07-13 18:19:58 +0300

    create component

--- js/app.js
+++ js/app.js
@@ -1,4 +1,36 @@
+var Comments = React.createClass({
+  render: function() {
+    return(
+      <div className="comments">
+        Нет новостей - комментировать нечего.
+      </div>
+    );
+  }
+})
+
+var News = React.createClass({
+  render: function() {
+    return(
+      <div className="news">
+        К сожалению, новостей нет.
+      </div>
+    );
+  }
+})
+
+var App = React.createClass({
+  render: function() {
+    return(
+      <div className="app">
+          Всем привет, я компонент App!
+          <News />
+          <Comments />
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  React.createElement('h1', null, 'Привет, Мир!'),
+  <App />,
   document.getElementById('root')
 )
